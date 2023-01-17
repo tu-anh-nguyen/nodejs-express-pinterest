dcc861fdbd68bc50518f1c9d9a574fd836b31598
Nguyễn Anh Tú <nguyenanhtubk16@gmail.com>
2023-01-18 00:18:00 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { sequelize } = require('./models');
 const { handleErrors } = require('./helpers/errors');
+const v1 = require('./routes/v1');
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static('.'));
 
 sequelize.sync({
   alter: true,
   // force: true,
 });
 
-const v1 = require('./routes/v1');
+// test
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
 app.use('/api/v1', v1);
 app.use(handleErrors);
